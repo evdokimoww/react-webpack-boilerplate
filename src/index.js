f7e937dda7f326e6c280c7678408a739540b08d7
evdokimoww <La15mCrSP1>
2022-03-24 14:58:22 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById('root')
+  <h1>Hello, world!</h1>,
+  document.getElementById('root'),
 );
